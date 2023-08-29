933b892082dddf7151500e271c003dc39b7fb8c2
krishnagithub03 <agrawalkrishna030@gmail.com>
2023-08-29 12:31:28 +0530

    money tracker (project based internship by iNeuron)

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <h2>
+      Keep Track Of Your Expense <span role="img" aria-label="wave">👋</span>&nbsp;
+    </h2>
+  )
+}
+
